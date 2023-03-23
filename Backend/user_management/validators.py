8ce77db2fc83b8ed87f2529b9874bdac32440962
Siyamak Abbasnezhad <siyamak1981@gmail.com>
2023-03-23 21:42:49 +0330

    #signin with  MetaMask by Django Restframework and React

---
+++ Backend/user_management/validators.py
@@ -0,0 +1,9 @@
+from inspect import signature
+
+from rest_framework import serializers
+
+
+class LoginPostValidator(serializers.Serializer):
+    signature = serializers.CharField()
+    web3_address = serializers.CharField()
+    
